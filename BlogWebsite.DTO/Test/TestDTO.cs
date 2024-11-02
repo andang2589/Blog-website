964e4b158276cb8996364a3421c756c6088c52fb
Binh An <19521170@gm.uit.edu.vn>
2024-11-02 15:31:56 +0700

    background-job initial commit

---
+++ BlogWebsite.DTO/Test/TestDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogWebsite.DTO.Test
+{
+    internal class TestDTO
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
